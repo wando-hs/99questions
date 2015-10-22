9d94d2265d2f1f9a89991a0ea711b68f61a46bd2
Wagner Leonardi <leonardiwagner@gmail.com>
2015-10-21 22:50:57 -0200

    pull request #2 fixes

--- src/Main.hs
+++
@@ -1,3 +0,0 @@
-module Main where
-main :: IO()
-main = print "99 questions an answer ain't one"
--- src/Problem07.hs
+++ src/Problem07.hs
@@ -19,7 +19,6 @@
 --  [1,2,3,4,5]
 --  *Main> flatten (List [])
 --  []
-
 module Problem07 where
 
 flatten :: [[a]] -> [a]
@@ -31,4 +30,3 @@ data NestedList a = Elem a | List [NestedList a] deriving (Show)
 flatten' :: NestedList a -> [a]
 flatten' (List []) = [] 
 flatten' (Elem el) = [el]
-flatten' (List (x:xs)) = (flatten' x) ++ (flatten' (List xs))

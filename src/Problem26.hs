f524b9e3046f51f1575dde48b5c8b55ab511ceeb
Fernando Oliveira <fernando@fholiveira.com>
2016-03-11 16:48:46 -0300

    Last version :shit:

--- src/Problem26.hs
+++ src/Problem26.hs
@@ -1,14 +1,14 @@
 import Data.List
 
-combinations'1 :: Int -> [a] -> [[a]]
-combinations'1 1 xs = map (\x -> [x]) xs
-combinations'1 n (x:xs) = map (\ys -> [x] ++ ys) $ combinations'1 (n-1) xs
-
 combinations :: Int -> [a] -> [[a]]
-combinations n r@(x:xs)
+combinations 1 xs = map (\x -> [x]) xs
+combinations n (x:xs) = map (\ys -> [x] ++ ys) $ combinations (n-1) xs
+
+combinations'1 :: Int -> [a] -> [[a]]
+combinations'1 n r@(x:xs)
     | n >= (length r) = [r]
-    | otherwise = list_combination ++ (combinations n xs)
-            where list_combination = combinations'1 n r
+    | otherwise = list_combination ++ (combinations'1 n xs)
+            where list_combination = combinations n r
 
 --groupera'1 :: [Int] -> [a] -> [a] -> [[[a]]]
 --groupera'1 xs fixed elements = map (\ ys -> [fixed] ++ ys)
@@ -19,21 +19,26 @@ combinations n r@(x:xs)
 --groupera [x] elements = [combinations x elements]
 --groupera (x:xs) elements = groupera'1 xs ys (elements \\ ys) ++ (combinations x elements)
 
---grouperinha :: [Int] -> [a] -> [[[a]]]
---grouperinha (x:xs) elements = combinations x elements
-                         -- ++ grouperinha xs ys
-                              --where ys = combinations x elements
+grouperinha :: [Int] -> [a] -> [[[a]]]
+grouperinha [] elements = []
+--grouperinha (x:xs) elements =  [combinations x elements] ++ grouperinha xs elements
 
+grouperinha (x:xs) elements = map (\c -> c ++ concat (grouperinha xs elements) ) 
+                             combinations x elements
+--                             ++ grouperinha xs els
+                             -- where ys = combinations x elements
+--                            where els = elements \\ f
 --groupera :: [Int] -> [a] -> [[[a]]]
 --groupera (x:xs) elements = grouperinha [x ++ xs] elements 
 
 gpr' :: Eq a => [Int] -> [[a]] -> [a] -> [[[a]]]
-gpr' [] fixed elements = [fixed]
 gpr' (x:xs) [] elements = gpr' xs (combinations x elements) elements
+
+gpr' [x] fixed elements = map (\ ys -> fixed ++ ys) [combinations x elements]
 gpr' c@(x:xs) (f:fs) elements =
     (map (\ ys -> [f] ++ ys) (gpr' xs (combinations x els) els))
     ++
-    (gpr' c fs elements)
+    (gpr' xs fs elements)
     where els = elements \\ f
 
 

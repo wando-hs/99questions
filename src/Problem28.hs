d92b7cc6921c413745896146e8c5b08bc6fb413a
Paulo Sousa <pauloj10@gmail.com>
2016-03-15 00:54:48 -0300

    Create test case to Problem28

--- src/Problem28.hs
+++ src/Problem28.hs
@@ -1,5 +1,7 @@
+module Problem28(lsort) where
+
 lsort :: [[a]] -> [[a]]
 lsort [] = []
-lsort (x:xs) = elements (<) ++ [x] ++ elements (>=)
-                        where elements cond = sorte $ filter (not . cond size . length) xs
+lsort (x:xs) = elements (<=) ++ [x] ++ elements (>)
+                        where elements cond = lsort $ filter (not . cond size . length) xs
                               size = length x

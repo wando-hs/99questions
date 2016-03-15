2e70e338727455f3d4f7910848169b6f8e67b527
Paulo Sousa <pauloj10@gmail.com>
2016-03-15 09:17:17 -0300

    Add test case to Problem28's secondx part

--- test/Problem28Spec.hs
+++ test/Problem28Spec.hs
@@ -8,8 +8,11 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-    describe "In item a" $ do
-        it "Sort list of lists of int" $
+    describe "In item a, sort based on itemm's length" $ do
+        it "List of lists of int" $
             Problem28.lsort [[1..4], [1,2], [1..4], [1..9], [20]] `shouldBe` [[20], [1,2], [1..4], [1..4], [1..9]]
-        it "Sort list of lists of char" $
+        it "List of lists of char" $
             Problem28.lsort ["abc","de","fgh","de","ijkl","mn","o"] `shouldBe` ["o","de","de","mn","abc","fgh","ijkl"]
+    describe "In item b, sort based on length frequency" $ do
+        it "Sort list of lists of char" $
+            Problem28.lsort ["abc", "de", "fgh", "de", "ijkl", "mn", "o"] `shouldBe` ["ijkl","o","abc","fgh","de","de","mn"]

b4d415c407cae75974675173d11d292b1089de8b
Paulo Sousa <pauloj10@gmail.com>
2016-03-17 00:18:34 -0300

    Update reference to problem28's b part

--- src/Problem28.hs
+++ src/Problem28.hs
@@ -1,7 +1,10 @@
-module Problem28(lsort) where
+module Problem28(lsort, lfsort) where
 
 lsort :: [[a]] -> [[a]]
 lsort [] = []
 lsort (x:xs) = elements (<=) ++ [x] ++ elements (>)
                         where elements cond = lsort $ filter (not . cond size . length) xs
                               size = length x
+
+lfsort :: [[a]] -> [[a]]
+lfsort xs = xs
--- test/Problem28Spec.hs
+++ test/Problem28Spec.hs
@@ -15,4 +15,4 @@ spec = do
             Problem28.lsort ["abc","de","fgh","de","ijkl","mn","o"] `shouldBe` ["o","de","de","mn","abc","fgh","ijkl"]
     describe "In item b, sort based on length frequency" $ do
         it "Sort list of lists of char" $
-            Problem28.lsort ["abc", "de", "fgh", "de", "ijkl", "mn", "o"] `shouldBe` ["ijkl","o","abc","fgh","de","de","mn"]
+            Problem28.lfsort ["abc", "de", "fgh", "de", "ijkl", "mn", "o"] `shouldBe` ["ijkl","o","abc","fgh","de","de","mn"]

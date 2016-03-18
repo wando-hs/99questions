7a540fd3854f1c45f9cfe144a8e25c96f8063df7
Paulo Sousa <pauloj10@gmail.com>
2016-03-18 01:25:27 -0300

    Problem28 Paulo' solution

--- src/Problem28.hs
+++ src/Problem28.hs
@@ -1,5 +1,8 @@
 module Problem28(lsort, lfsort) where
 
+import Data.List(sortBy)
+import Data.Function(on)
+
 lsort :: [[a]] -> [[a]]
 lsort [] = []
 lsort (x:xs) = elements (<=) ++ [x] ++ elements (>)
@@ -7,4 +10,18 @@ lsort (x:xs) = elements (<=) ++ [x] ++ elements (>)
                               size = length x
 
 lfsort :: [[a]] -> [[a]]
-lfsort xs = xs
+lfsort xs = concatMap (takeListsWithSize xs) $ sortBy (compare `on` snd) sizesGrouped 
+                    where sizes = foldl (\ s current -> s ++ [length current]) [] xs
+                          sizesGrouped = foldl (\ group item -> lInsert item group) [] sizes
+
+takeListsWithSize :: [[a]] -> (Int, Int) -> [[a]]
+takeListsWithSize [] _ = []
+takeListsWithSize (x:xs) s@(size, _)
+    | (length x) == size = x : takeListsWithSize xs s
+    | otherwise = takeListsWithSize xs s
+
+lInsert :: Int -> [(Int, Int)] -> [(Int, Int)]
+lInsert size [] = [(size, 1)]
+lInsert size ((s, frequency):xs) 
+    | size == s = (s, frequency + 1) : xs
+    | otherwise = (s, frequency) : lInsert size xs

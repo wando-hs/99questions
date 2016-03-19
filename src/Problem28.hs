20b9d678aa512d2e4a2e9310f680e0931a3d045b
Paulo Sousa <pauloj10@gmail.com>
2016-03-19 11:14:16 -0300

    dojo solution

--- src/Problem28.hs
+++ src/Problem28.hs
@@ -9,19 +9,11 @@ lsort (x:xs) = elements (<=) ++ [x] ++ elements (>)
                         where elements cond = lsort $ filter (not . cond size . length) xs
                               size = length x
 
+
 lfsort :: [[a]] -> [[a]]
-lfsort xs = concatMap (takeListsWithSize xs) $ sortBy (compare `on` snd) sizesGrouped 
-                    where sizes = foldl (\ s current -> s ++ [length current]) [] xs
-                          sizesGrouped = foldl (\ group item -> lInsert item group) [] sizes
+lfsort [] = []
+lfsort l@(x:xs) = elements (withFrequency (<)) ++ [x] ++ elements (withFrequency (>=))
+                        where elements cond = lfsort $ filter cond xs
+                              withFrequency pred y = pred (frequency y) (frequency x)
+                              frequency el = length $ filter (length el ==) $ map length l
 
-takeListsWithSize :: [[a]] -> (Int, Int) -> [[a]]
-takeListsWithSize [] _ = []
-takeListsWithSize (x:xs) s@(size, _)
-    | (length x) == size = x : takeListsWithSize xs s
-    | otherwise = takeListsWithSize xs s
-
-lInsert :: Int -> [(Int, Int)] -> [(Int, Int)]
-lInsert size [] = [(size, 1)]
-lInsert size ((s, frequency):xs) 
-    | size == s = (s, frequency + 1) : xs
-    | otherwise = (s, frequency) : lInsert size xs
